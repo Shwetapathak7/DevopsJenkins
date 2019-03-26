2943daddf4f6820c45d25f9da735225344c33c84
Pathak <shweta.b.pathak@accenture.com>
2019-03-26 14:44:46 +0530

    Maven Project

--- src/test/java/FIrstPackage/NewTest.java
+++ src/test/java/FIrstPackage/NewTest.java
@@ -1,10 +1,25 @@
 package FIrstPackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
-System.out.println("Hello");
+	  System.out.println("This is my test project");
+	  System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+	  //launch chrome browser
+	  WebDriver driver=new ChromeDriver();
+	  //open the application
+	  driver.get("http://newtours.demoaut.com/");
+	  
+	  //enter username
+	  driver.findElement(By.name("userName")).sendKeys("mercury");
+	  driver.findElement(By.name("password")).sendKeys("mercury");
+	  driver.findElement(By.name("login")).click();
+	  driver.close();
+	  
   }
-}
+} 
